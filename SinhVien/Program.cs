15419b6d7f6bc0058e95e0a4e6a04ebff510ed36
Truong <truongphamxuan724@gmail.com>
2023-04-21 23:24:30 +0700

    alo

---
+++ SinhVien/Program.cs
@@ -0,0 +1,114 @@
+﻿using SinhVien;
+using System;
+
+class program
+{
+    static List<SinhVienA> dsSinhVien = new List<SinhVienA>();
+    static void Nhap()
+    {
+        SinhVienA sinhVien = new SinhVienA();
+        sinhVien.Nhap();
+        dsSinhVien.Add(sinhVien);
+    }
+    static void HienThi()
+    {
+        SinhVienA.inTieuDe();
+        foreach(SinhVienA sv in dsSinhVien)
+        {
+            sv.inDL();
+        }
+    }
+    static void HienThiTheoTongDiem()
+    {
+        Console.Write("Nhập Điểm: ");
+        double diem = double.Parse(Console.ReadLine());
+        SinhVienA.inTieuDe();
+        foreach(SinhVienA sv in dsSinhVien)
+        {
+            if(diem <= sv.TongDiem)
+            {
+                sv.inDL();
+            }
+        }
+    }
+    static void HienThiTheoDiaChi()
+    {
+        Console.Write("Nhập Địa Chỉ: ");
+        string diachi = Console.ReadLine();
+        SinhVienA.inTieuDe();
+        foreach (SinhVienA sv in dsSinhVien)
+        {
+            if (diachi == sv.DiaChi)
+            {
+                sv.inDL();
+            }
+        }
+    }
+    static void TimKiemTheoSBD()
+    {
+        Console.Write("Nhập SBD: ");
+        string sbd = Console.ReadLine();
+        SinhVienA.inTieuDe();
+        foreach (SinhVienA sv in dsSinhVien)
+        {
+            if (sbd == sv.SBD)
+            {
+                sv.inDL();
+            }
+        }
+    }
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        int chon;
+        do
+        {
+            Console.WriteLine("MENU");
+            Console.WriteLine("1. Nhập thông tin 1 thí sinh");
+            Console.WriteLine("2. Hiển thị danh sách các thí sinh");
+            Console.WriteLine("3. Hiển thị các sinh viên theo tổng điểm");
+            Console.WriteLine("4. Hiển thị các sinh viên theo địa chỉ");
+            Console.WriteLine("5. Tìm Kiếm theo số báo danh");
+            Console.WriteLine("6. Kết thúc chương trình");
+            Console.WriteLine("7. Truyền file");
+            Console.WriteLine("Nhập lựa chọn: ");
+            chon = int.Parse(Console.ReadLine());
+            switch (chon)
+            {
+                case 1:
+                    Nhap();
+                    break;
+                case 2:
+                    HienThi();
+                    break;
+                case 3:
+                    HienThiTheoTongDiem();
+                    break;
+                case 4:
+                    HienThiTheoDiaChi();
+                    break;
+                case 5:
+                    TimKiemTheoSBD();
+                    break;
+                case 6:
+                    Console.WriteLine("Chương trình kết thúc.");
+                    break;
+                case 7:
+                    string filePath= "SinhVien.txt";
+                    using (StreamWriter writer = new StreamWriter(filePath))
+                    {
+                        writer.WriteLine("{0, -15}{1, -15}{2, -15}{3, -15}{4, -15}{5, -15}{6, -15}{7, -15}", "SBD", "Họ Tên", "Địa Chỉ", "Toán", "Lý", "Hóa", "Điểm ƯT", "Tổng Điểm");
+                        foreach(SinhVienA sv in dsSinhVien)
+                        {
+                            writer.WriteLine("{0, -15}{1, -15}{2, -15}{3, -15}{4, -15}{5, -15}{6, -15}{7, -15}", sv.SBD, sv.HoTen, sv.DiaChi, sv.Toan, sv.Ly, sv.Hoa, sv.DiemUT, sv.TongDiem);
+                        }
+                    }
+
+                    break;
+                default:
+                    Console.WriteLine("Lụa Chọn Không Hợp Lệ");
+                    break;
+            }
+        } while (chon != 6);
+    }
+}
---
+++ SinhVien/SinhVienA.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinhVien
+{
+    class SinhVienA
+    {
+        public String SBD { get; set; }
+        public String HoTen { get; set; }
+        public String DiaChi { get; set; }
+        public double Toan { get; set; }
+        public double Ly { get; set; }
+        public double Hoa { get; set; }
+        public double DiemUT { get; set; }
+        public double TongDiem
+        {
+            get { return Toan + Ly + Hoa + DiemUT; }
+        }
+        public SinhVienA() { }
+        public SinhVienA(string sBD, string hoTen, string diaChi, double toan, double ly, double hoa, double diemUT)
+        {
+            SBD = sBD;
+            HoTen = hoTen;
+            DiaChi = diaChi;
+            Toan = toan;
+            Ly = ly;
+            Hoa = hoa;
+            DiemUT = diemUT;
+        }
+        public void Nhap()
+        {
+            Console.Write("Số Báo Danh: ");
+            SBD = Console.ReadLine();
+            Console.Write("Họ Tên: ");
+            HoTen = Console.ReadLine();
+            Console.Write("Địa Chỉ: ");
+            DiaChi = Console.ReadLine();
+            Console.Write("Điểm Toán: ");
+            Toan = double.Parse(Console.ReadLine());
+            Console.Write("Điểm Lý: ");
+            Ly = double.Parse(Console.ReadLine());
+            Console.Write("Điểm Hóa: ");
+            Hoa = double.Parse(Console.ReadLine());
+            Console.Write("Điểm ưu Tiên: ");
+            DiemUT = double.Parse(Console.ReadLine());
+        }
+        public static void inTieuDe()
+        {
+            Console.WriteLine("{0, -15}{1, -15}{2, -15}{3, -15}{4, -15}{5, -15}{6, -15}{7, -15}", "SBD", "Họ Tên", "Địa Chỉ", "Toán", "Lý","Hóa", "Điểm ƯT", "Tổng Điểm");
+        }
+        public void inDL()
+        {
+            Console.WriteLine("{0, -15}{1, -15}{2, -15}{3, -15}{4, -15}{5, -15}{6, -15}{7, -15}", SBD,HoTen, DiaChi,Toan, Ly, Hoa, DiemUT, TongDiem ) ;
+        }
+    }
+
+}
